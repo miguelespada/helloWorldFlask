959a9b0f636277a53e9b6d4b17ee53dc625ef14c
miguelespada <miguel.valero.espada@gmail.com>
2017-01-22 17:38:47 +0100

    add predictor

--- index.py
+++ index.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from flask import request
 from random import randint
 from time import time
+from humbledb import Mongo, Document
+import cPickle
+
+
+with open('classifier.pkl', 'rb') as fid:
+    regr_loaded = cPickle.load(fid)
+
+class OP(Document):
+  config_database = 'operation'
+  config_collection = 'add'
 
 app = Flask(__name__)
 
 @app.route('/',methods = ["GET", "POST"])
 def hello_world():
   if request.method == 'POST':
-    values = {}
-    values["result"] = int(request.form['value'])
-    values["v1"] = int(request.form['v1'])
-    values["v2"] = int(request.form['v2'])
-    values["ellapsed"] = time() - float(request.form['timestamp'])
-    if values["v1"] + values["v2"] == values["result"]:
-      return render_template("result.html", value = values)
+    op = OP()
+    op["result"] = request.form['value']
+    op["v1"] = request.form['v1']
+    op["v2"] = request.form['v2']
+    op["ellapsed"] = str(time() - float(request.form['timestamp']))
+    if int(op["v1"]) + int(op["v2"]) == int(op["result"]):
+      with Mongo:
+        OP.insert(op)
+      op["prediction"] = regr_loaded.predict([int(op["v1"]), int(op["v2"]),  int(op["v1"]) < 10 or int(op["v2"]) < 10])[0]
+      return render_template("result.html", value = op)
     else:
-      values["timestamp"] = float(request.form['timestamp'])
-      return render_template("form.html", vs = values)
+      op["timestamp"] = float(request.form['timestamp'])
+      return render_template("form.html", vs = op)
   if request.method == 'GET':
     values = {}
     values["v1"] = randint(0, 100)
@@ -26,3 +39,22 @@ def hello_world():
     values["ellapsed"] = 0
     return render_template("form.html", vs = values)
 
+@app.route('/list',methods = ["GET"])
+def list():
+  with Mongo:
+    ops = OP.find()
+    ss = ""
+    for o in ops:
+      try:
+        ss += o["v1"] 
+        ss += ";" 
+        ss += o["v2"] 
+        ss += ";" 
+        ss += o["ellapsed"] 
+        ss += "\n"
+      except Exception as e:
+        pass
+    output = make_response(ss)
+    output.headers["Content-Disposition"] = "attachment; filename=export.csv"
+    output.headers["Content-type"] = "text/csv"
+    return output
