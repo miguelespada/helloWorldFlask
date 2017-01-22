f22cf0d35174dbb2030675e18601c1d9cb93dfe0
miguelespada <miguel.valero.espada@gmail.com>
2017-01-22 16:06:41 +0100

    allow resubmission

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request
 from random import randint
+from time import time
 
 app = Flask(__name__)
 
@@ -11,10 +12,17 @@ def hello_world():
     values["result"] = int(request.form['value'])
     values["v1"] = int(request.form['v1'])
     values["v2"] = int(request.form['v2'])
-    return render_template("result.html", value = values)
+    values["ellapsed"] = time() - float(request.form['timestamp'])
+    if values["v1"] + values["v2"] == values["result"]:
+      return render_template("result.html", value = values)
+    else:
+      values["timestamp"] = float(request.form['timestamp'])
+      return render_template("form.html", vs = values)
   if request.method == 'GET':
     values = {}
     values["v1"] = randint(0, 100)
     values["v2"] = randint(0, 100)
+    values["timestamp"] = time()
+    values["ellapsed"] = 0
     return render_template("form.html", vs = values)
 
